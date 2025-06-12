96e148c9ee414494c65587f0b2328e0c47f994fc
AbhishekPatel07 <abhi409043@gmail.com>
2025-06-12 21:57:35 +0930

    Fix question14.sh: fetch & checkout update1 from upstream

--- question14.sh
+++ question14.sh
@@ -1,3 +1,4 @@
-git remote add upstream https://github.com/ian-knight-uofa/git-practice-04.git\ 
-git fetch upstream update1:update1\ 
-git checkout update1
+git remote remove upstream 2>/dev/null || true
+git remote add upstream https://github.com/ian-knight-uofa/git-practice-04.git
+git fetch upstream
+git checkout -b update1 upstream/update1
